25f64afc60cffbd55b3f0373971d954ba6e98776
ryo-hidaka <66785120+ryo-hidaka@users.noreply.github.com>
2022-05-17 16:32:35 +0900

    再レンダリングとexport

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+/** eslint react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import ColorfullMessage from "./components/ColorfullMassage";
 
 const App = () => {
+  console.log("saisiho");
+  const [num, setNum] = useState(0);
+  const [faseShowFlag, setFaseShowFlag] = useState(false);
+
+  const onClickSwitchShow = () => {
+    setFaseShowFlag(!faseShowFlag);
+  };
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faseShowFlag || setFaseShowFlag(true);
+      } else {
+        faseShowFlag && setFaseShowFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -13,7 +31,10 @@ const App = () => {
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">元気です！</ColorfullMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShow}>on/of</button>
       <p>{num}</p>
+      {faseShowFlag && <p>(..)</p>}
     </>
   );
 };
